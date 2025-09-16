0af4b859d85acb922f88623f0c100775357beece
THOANHDO <106481190+THOANHDO@users.noreply.github.com>
2025-09-16 16:00:33 +0700

    Fix Codable conformance for Transaction

--- Sources/FinanceToToApp/Models/Transaction.swift
+++ Sources/FinanceToToApp/Models/Transaction.swift
@@ -12,6 +12,23 @@ struct Transaction: Identifiable, Codable, Hashable {
     var location: CLLocationCoordinate2D?
     var isIncome: Bool
 
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case date
+        case amount
+        case category
+        case merchant
+        case notes
+        case receipt
+        case location
+        case isIncome
+    }
+
+    private enum LocationCodingKeys: String, CodingKey {
+        case latitude
+        case longitude
+    }
+
     init(
         id: UUID = UUID(),
         date: Date = .now,
@@ -32,6 +49,49 @@ struct Transaction: Identifiable, Codable, Hashable {
         self.receipt = receipt
         self.location = location
         self.isIncome = isIncome
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        id = try container.decode(UUID.self, forKey: .id)
+        date = try container.decode(Date.self, forKey: .date)
+        amount = try container.decode(Decimal.self, forKey: .amount)
+        category = try container.decode(TransactionCategory.self, forKey: .category)
+        merchant = try container.decode(String.self, forKey: .merchant)
+        notes = try container.decodeIfPresent(String.self, forKey: .notes) ?? ""
+        receipt = try container.decodeIfPresent(Receipt.self, forKey: .receipt)
+
+        if container.contains(.location) {
+            let locationContainer = try container.nestedContainer(keyedBy: LocationCodingKeys.self, forKey: .location)
+            let latitude = try locationContainer.decode(CLLocationDegrees.self, forKey: .latitude)
+            let longitude = try locationContainer.decode(CLLocationDegrees.self, forKey: .longitude)
+            location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        } else {
+            location = nil
+        }
+
+        isIncome = try container.decodeIfPresent(Bool.self, forKey: .isIncome) ?? false
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(id, forKey: .id)
+        try container.encode(date, forKey: .date)
+        try container.encode(amount, forKey: .amount)
+        try container.encode(category, forKey: .category)
+        try container.encode(merchant, forKey: .merchant)
+        try container.encode(notes, forKey: .notes)
+        try container.encodeIfPresent(receipt, forKey: .receipt)
+
+        if let location {
+            var locationContainer = container.nestedContainer(keyedBy: LocationCodingKeys.self, forKey: .location)
+            try locationContainer.encode(location.latitude, forKey: .latitude)
+            try locationContainer.encode(location.longitude, forKey: .longitude)
+        }
+
+        try container.encode(isIncome, forKey: .isIncome)
     }
 }
 
