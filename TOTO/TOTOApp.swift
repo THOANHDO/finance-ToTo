7995417ab91dae50239544fca771ee847d848db3
thoanhdo <thoanhdo21ck5100@gmail.com>
2025-10-08 13:56:58 +0700

    Initial Commit

---
+++ TOTO/TOTOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOTOApp.swift
+//  TOTO
+//
+//  Created by Thơ Anh Đỗ on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TOTOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
